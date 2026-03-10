a00d6813b718e1974fc29e86d4727a380262ccce
Sebastian Sobczyński <126495731+Orleann@users.noreply.github.com>
2026-03-10 16:11:29 +0100

    Make Hello accept props and add Footer
    Update Hello to accept props and display name/age (with console logging); add a Footer component that receives a student prop; and update App to define sample variables and pass props to Hello and Footer (including an expression for the second Hello's age). These changes demonstrate basic prop usage and component composition.

--- Crash course/src/App.jsx
+++ Crash course/src/App.jsx
@@ -1,18 +1,35 @@
-const Hello = () => {
+const Hello = (props) => {
   const now = new Date()
+  console.log(props)
   console.log(now)
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {props.name}, you are {props.age}</h1>
       <p>Today is {now.toDateString()}</p>
     </div>
   )
 }
 
+const Footer = (maker) => {
+  return(
+    <div>
+      <p>Footer, crash course made by {maker.student}</p>
+    </div>
+  )
+}
+
 const App = () => {
+
+  const name = "Sebastian"
+  const student = "Sebastian, student informatyki DX"
+  const age = 21
+  const a = 60
+  const b = 7
   return (
     <div>
-      <Hello />
+      <Hello name = {name} age = {age} />
+      <Hello name = "Kolega Mariusz" age = {a + b}/>
+      <Footer student = {student} />
     </div>
   )
 }
